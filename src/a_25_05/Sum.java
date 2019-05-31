a6faa165636bab51bd838416757ebe597df8d705
VladVlad5960 <51019607+VladVlad5960@users.noreply.github.com>
2019-05-31 12:50:56 +0300

    Add files via upload

---
+++ src/a_25_05/Sum.java
@@ -0,0 +1,16 @@
+package a_25_05;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        int n = 100;
+        int sum = 0;
+
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ src/i_25_05/Factorial.java
@@ -0,0 +1,16 @@
+package i_25_05;
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        long num;
+        long fact=1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        num = input.nextInt();
+        for (long i=2;i<=num; i++){
+            fact=fact*i;
+        }
+
+        System.out.println("Factorial: "+fact);
+    }
+    }
